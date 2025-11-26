57a1f48dc7b63215321f75733bee25e3109b9f05
billah2025 <m.b.siam2008@gmail.com>
2025-11-27 00:22:04 +0600

    multi user

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -15,7 +15,7 @@ export default function RegisterPage() {
     setError("");
 
     try {
-      const res = await fetch(`http://localhost:5000/api/auth/register`, {
+      const res = await fetch(`https://printapi-godr.onrender.com/api/auth/register`, {
         method: "POST",
         headers: { "Content-Type": "application/json",
           "x-api-key": process.env.NEXT_PUBLIC_API_KEY!,
